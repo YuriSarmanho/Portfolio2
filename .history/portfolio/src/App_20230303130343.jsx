3b17a790cd899fb01cbbb984aca7bfba9a59041d
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2023-03-03 13:13:01 -0300

    add links to the navbar buttons to redirect to the sections

--- .history/portfolio/src/App_20230303130343.jsx
+++ .history/portfolio/src/App_20230303130343.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./compo/Header";
 
 function App() {
   return (
---
+++ .history/portfolio/src/components/Header/index_20230303125502.jsx
@@ -0,0 +1,45 @@
+import logo from "../../img/logo.svg";
+import MobileNav from "../../components/MobileNav";
+import "./header.css";
+
+const header = () => {
+  return (
+    <header>
+      <a href="/" className="logo">
+        <img src={logo} alt="the site's logo" />
+      </a>
+      <nav>
+        <ul className="nav-ul">
+          <li className="nav-li">
+            <a href="#home" className="nav-a">
+              Home
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/about" className="nav-a">
+              About
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/projects" className="nav-a">
+              Projects
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/contact" className="nav-a">
+              Contact
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/contact" className="nav-a resume">
+              Currículo
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <MobileNav/>
+    </header>
+  );
+};
+
+export default header;
