f37b7a02135a55a06ee537aba381c4a9c8610996
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2023-03-07 15:43:36 -0300

    home section content

--- portfolio/src/sections/Home/index.jsx
+++ portfolio/src/sections/Home/index.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./home.css";
 
 const Home = () => {
-  return <section id="home">Home</section>;
+  return (
+    <section id="home" className="flex">
+      <div className="home-content">
+        <p id="">Olá, meu nome é</p>
+        <h1 id="myName">
+          Yuri Sarmanho <br />
+          Monteiro
+        </h1>
+        <p>sou desenvolvedor Front-end</p>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
