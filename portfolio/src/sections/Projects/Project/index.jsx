4f4eb0454767419c0a6b2268cabf2fb02005838b
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2023-03-11 17:01:34 -0300

    projects area added with the project compont

--- portfolio/src/sections/Projects/Project/index.jsx
+++ portfolio/src/sections/Projects/Project/index.jsx
@@ -1,31 +1,25 @@
 import fileIcon from "../../../img/fileIcon.svg";
-import linkIcon from "../../../img/linkIcon.svg";
 import { AiFillGithub } from "react-icons/ai";
 import { RxExternalLink } from "react-icons/rx";
 import "./project.css";
 
-const Project = () => {
+const Project = (props) => {
   return (
     <div className="project-container">
       <div className="project-header flex">
         <img src={fileIcon} alt="File icon" className="project_file-icon" />
         <div className="project-links">
-          <a href="#">
+          <a href={props.githubLink}>
             <AiFillGithub className="github-icon" />
           </a>
-          <a href="#">
-            <RxExternalLink/>
+          <a href={props.pageLink}>
+            <RxExternalLink />
           </a>
         </div>
       </div>
       <div className="project-info">
-        <h3>MovieStar</h3>
-        <div className="project-description">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambl.
-        </div>
+        <h3>{props.name}</h3>
+        <div className="project-description">{props.description}</div>
       </div>
     </div>
   );
--- portfolio/src/sections/Projects/index.jsx
+++ portfolio/src/sections/Projects/index.jsx
@@ -3,9 +3,89 @@ import Project from "./Project";
 import "./projects.css";
 
 const Projects = () => {
-  return <section id="projects">
-    {/* <Project/> */}
-  </section>;
+  const projectsData = [
+    {
+      name: "hamburguerMenu",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 0,
+    },
+    {
+      name: "MovieStar",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 1,
+    },
+    {
+      name: "Calculadora",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 2,
+    },
+    {
+      name: "Quiz",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 3,
+    },
+    {
+      name: "Conversor de Medida",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 4,
+    },
+    {
+      name: "CadastroFirebase",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 5,
+    },
+    {
+      name: "CadastroFirebase",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 5,
+    },
+    {
+      name: "CadastroFirebase",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambl.",
+      pageLink: "#",
+      githubLink: "#",
+      id: 5,
+    },
+  ];
+
+  return (
+    <section id="projects" className="flex">
+      <h2>Projetos</h2>
+      <div className="projects-container">
+        {projectsData.map((project) => (
+          <Project
+            name={project.name}
+            description={project.description}
+            pageLink={project.pageLink}
+            githubLink={project.githubLink}
+            key={project.id}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
