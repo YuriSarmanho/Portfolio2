6d7e4fabb065b34559c4090e7905add08957bac5
yuriSarmanho <yuri.sarmanho@hotmail.com>
2024-01-21 02:52:44 -0300

    change the project display layout

--- portfolio/src/sections/Projects/Project/index.jsx
+++ portfolio/src/sections/Projects/Project/index.jsx
@@ -3,18 +3,34 @@ import { AiFillGithub } from "react-icons/ai";
 import { RxExternalLink } from "react-icons/rx";
 import "./project.css";
 
+
+// const Project = (props) => {
+//   return (
+//     <div className="project-container">
+//       <div className="project-header flex">
+//         <img src={fileIcon} alt="File icon" className="project_file-icon" />
+//         <div className="project-links">
+//           <a href={props.githubLink} target="_blank">
+//             <AiFillGithub className="github-icon" />
+//           </a>
+//           {/* <a href={props.pageLink}>
+//             <RxExternalLink />
+//           </a> */}
+//         </div>
+//       </div>
+//       <div className="project-info">
+//         <h3>{props.name}</h3>
+//         <div className="project-description">{props.description}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const Project = (props) => {
   return (
     <div className="project-container">
-      <div className="project-header flex">
-        <img src={fileIcon} alt="File icon" className="project_file-icon" />
-        <div className="project-links">
-          <a href={props.githubLink} target="_blank">
-            <AiFillGithub className="github-icon" />
-          </a>
-          {/* <a href={props.pageLink}>
-            <RxExternalLink />
-          </a> */}
+      <div className="project-img-container flex">
+        <div className="project-header flex">
         </div>
       </div>
       <div className="project-info">
@@ -24,5 +40,4 @@ const Project = (props) => {
     </div>
   );
 };
-
 export default Project;
